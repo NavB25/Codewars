5e40834b6dd43f72644a6ad125a31d542050610b
Navkaran Bola <99494360+NavB25@users.noreply.github.com>
2022-04-06 10:26:45 -0400

    Delete Replace_With_Alphabet_Position.py

--- Python_Challenges/6_Kyu/Replace_With_Alphabet_Position.py
+++
@@ -1,9 +0,0 @@
-def alphabet_position(text):
-    
-    import string
-    
-    alpha_lst = list(string.ascii_lowercase)
-    
-    numbered_lst = [str(alpha_lst.index(letter) + 1) for letter in text.lower() if letter.isalpha()]  
-    
-    return " ".join(numbered_lst)
